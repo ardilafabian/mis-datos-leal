35eb3d7bd2ab761a7f75040bf5e380b0a77e1e4c
Fabian Ardila <fabianra97@gmail.com>
2020-03-31 20:23:33 -0500

    Separate transactions to a new microservice

--- transaction/index.js
+++ transaction/index.js
@@ -2,8 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const config = require('../config');
-const user = require('./components/user/network');
-const auth = require('./components/auth/network');
+const transaction = require('./components/transaction/network');
 const errors = require('../network/errors');
 
 const app = express();
@@ -11,11 +10,10 @@ const app = express();
 app.use(bodyParser.json());
 
 // ROUTES
-app.use('/user', user);
-app.use('/auth', auth);
+app.use('/transaction', transaction);
 
 app.use(errors);
 
-app.listen(config.api.port, () => {
-    console.log('Listening in port', config.api.port);
+app.listen(config.transaction.port, () => {
+    console.log('Servicio de transacciones esuchando en puerto', config.transaction.port);
 });
