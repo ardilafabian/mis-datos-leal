57651c13c3f49320e8d6e1333c06ee84a4928820
Fabian Ardila <fabianra97@gmail.com>
2020-04-27 13:15:13 -0500

    Configure deploy files

--- api/components/user/index.js
+++ api/components/user/index.js
@@ -1,5 +1,12 @@
-//const store = require('../../../store/mysql');
-const store = require('../../../mysql/store/remote-mysql');
+//const store = require('../../../store/mysql')
+const config = require('../../../config');
+
+let store;
+if (config.remoteDB === true) {
+    store = require('../../../mysql/store/remote-mysql');
+} else {
+    store = require('../../../mysql/store/mysql'); 
+}
 const ctrl = require('./controller');
 
 module.exports = ctrl(store);
--- export/components/export/index.js
+++ export/components/export/index.js
@@ -1,4 +1,12 @@
 const store = require('../../../mysql/store/remote-mysql');
+
+let store;
+if (config.remoteDB === true) {
+    store = require('../../../mysql/store/remote-mysql');
+} else {
+    store = require('../../../mysql/store/mysql'); 
+}
+
 const ctrl = require('./controller');
 
 module.exports = ctrl(store);
